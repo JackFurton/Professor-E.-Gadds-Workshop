a64ea6375f11a9b979c7288a51e2176fa890e488
Jack Furton <jack.furton@phoenix-opsgroup.com>
2023-07-06 10:26:46 -0400

    begin python commits

---
+++ python/class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
