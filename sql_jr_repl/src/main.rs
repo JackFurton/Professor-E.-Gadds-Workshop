aa19c5658974d59f82f1acc38fffc892af4e358a
Jack Furton <jack.furton@phoenix-opsgroup.com>
2023-09-22 13:51:07 -0400

    RustSullivan

---
+++ sql_jr_repl/src/main.rs
@@ -0,0 +1,32 @@
+use rustyline::error::ReadlineError;
+use rustyline::{Editor, Result};
+const HISTORY_FILE: &str = "./history.txt";
+fn main() -> Result<()> {
+    let mut rl = Editor::<()>::new()?;
+    if rl.load_history(HISTORY_FILE).is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                // CTRL-C so just skip
+            },
+            Err(ReadlineError::Eof) => {
+                // CTRL-D so exit
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+    rl.save_history(HISTORY_FILE)
+}
+
+// https://johns.codes/blog/build-a-db/part01
