714c1879f127aec3cab6516a6ff4770428761480
Jack Furton <jack.furton@phoenix-opsgroup.com>
2024-01-02 15:25:46 -0500

    gofmt -w .

--- golang/interpreter/lexer/lexer_test.go
+++ golang/interpreter/lexer/lexer_test.go
@@ -6,38 +6,57 @@ import (
 	"interpreter/token"
 )
 
-
 func (l *Lexer) NextToken() token.Token {
-    var tok token.Token
+	var tok token.Token
 
-    switch l.ch {
-    case '=':
-        tok = newToken(token.ASSIGN, l.ch)
-    case ';':
-        tok = newToken(token.SEMICOLON, l.ch)
-    case '(':
-        tok = newToken(token.LPAREN, l.ch)
-    case ')':
-        tok = newToken(token.RPAREN, l.ch)
-    case ',':
-        tok = newToken(token.COMMA, l.ch)
-    case '+':
-        tok = newToken(token.PLUS, l.ch)
-    case '{':
-        tok = newToken(token.LBRACE, l.ch)
-    case '}':
-        tok = newToken(token.RBRACE, l.ch)
-    case 0:
-        tok.Literal = ""
-        tok.Type = token.EOF
-    }
+	switch l.ch {
 
-    l.readChar()
-    return tok
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
-    return token.Token{Type: tokenType, Literal: string(ch)}
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
 type Lexer struct {
@@ -64,19 +83,55 @@ func (l *Lexer) readChar() {
 }
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+	let ten = 10;
+
+	let add = fn(x,y) {
+	x + y;
+	};
+
+	let result = add(five,ten);
+	`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
 		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
