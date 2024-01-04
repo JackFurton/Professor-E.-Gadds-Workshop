6e175599a4e4a73c25e89e00db88882a4fd5c584
Jack Furton <jack.furton@phoenix-opsgroup.com>
2024-01-03 19:31:33 -0500

    npos

---
+++ cpp/aoc1/main.cc
@@ -0,0 +1,38 @@
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <cctype>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ifstream ins("file2.txt");
+    if (!ins) {
+        cout << "File not found.\n";
+        exit(1);
+    }
+    int total = 0;
+    string s;
+    while (getline(ins, s)) {
+        int first_digit = 0;
+        for (auto it = s.begin(); it != s.end(); it++) {
+            char ch = *it;
+            if (isdigit(ch)) {
+                first_digit = ch - '0';
+                break;
+            }
+        }
+        int last_digit = 0;
+        for (auto it = s.rbegin(); it != s.rend(); it++) {
+            char ch = *it;
+            if (isdigit(ch)) {
+                last_digit = ch - '0';
+                break;
+            }
+        }
+        int number = first_digit * 10 + last_digit;
+        total += number;
+        cout << total << endl;
+    }
+}
---
+++ cpp/cov/main.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    string n;
+    cin >> n;
+
+    if (n.find ("COV") != string::npos) {
+        cout << "Veikur\n";
+    } else {
+        cout << "Ekki veikur!\n";
+    }
+}
+/*
+The std::string::find function returns the position of the first occurrence of the substring in the string. If the substring is not found, it returns std::string::npos.
+
+n.find("COV") by itself will return a position if "COV" is found, or std::string::npos if it's not found. But we can't use this value directly in an if statement, because if expects a boolean condition.
+
+In C++, any non-zero value is considered true and zero is considered false. std::string::npos is a non-zero value (actually, it's the maximum value that can be held by size_t), so if "COV" is not found and n.find("COV") returns std::string::npos, the if statement would consider it true, which is not what we want.
+
+we need to compare the result of n.find("COV") to std::string::npos. If "COV" is found, n.find("COV") will return its position (which is not equal to std::string::npos), and the if statement will be true. If "COV" is not found, n.find("COV") will return std::string::npos (which is equal to std::string::npos), and the if statement will be false.
+*/
+
+//tldr: checking size_t of the string
+
+//use the find to locate "COV"
+
+//if not we return npos which hits the else statement
